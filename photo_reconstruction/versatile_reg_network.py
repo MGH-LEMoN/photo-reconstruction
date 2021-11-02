310840b6a189ff25635e331b1e0a6159ea3bfc7e
Juan Eugenio Iglesias <e.iglesias@ucl.ac.uk>
2021-11-02 11:34:42 -0400

    Cleaned up comments a bit

--- photo_reconstruction/versatile_reg_network.py
+++ photo_reconstruction/versatile_reg_network.py
@@ -4,48 +4,6 @@ import torch.nn.functional as nnf
 import numpy as np
 import scipy.ndimage
 
-
-# def slice_wise_LNCC(I, J, win_semilen=3):
-#
-#     # set window size
-#
-#     I_rearranged = torch.unsqueeze(torch.unsqueeze(I, dim=0), dim=0)
-#     J_rearranged = torch.unsqueeze(torch.unsqueeze(J, dim=0), dim=0)
-#
-#     win = [1 + 2 * win_semilen, 1 + 2 * win_semilen, 1]
-#     sum_filt = torch.ones([1, 1, *win]).to(I.device)
-#     pad_no = win_semilen
-#     stride = (1, 1, 1)
-#     padding = (pad_no, pad_no, 1)
-#
-#     # get convolution function
-#     conv = getattr(torch.nn.functional, 'conv3d')
-#
-#     # compute CC squares
-#     I2 = I_rearranged * I_rearranged
-#     J2 = J_rearranged * J_rearranged
-#     IJ = I_rearranged * J_rearranged
-#
-#     I_sum = conv(I_rearranged, sum_filt, stride=stride, padding=padding)
-#     J_sum = conv(J_rearranged, sum_filt, stride=stride, padding=padding)
-#     I2_sum = conv(I2, sum_filt, stride=stride, padding=padding)
-#     J2_sum = conv(J2, sum_filt, stride=stride, padding=padding)
-#     IJ_sum = conv(IJ, sum_filt, stride=stride, padding=padding)
-#
-#     win_size = np.prod(win)
-#     u_I = I_sum / win_size
-#     u_J = J_sum / win_size
-#
-#     cross = IJ_sum - u_J * I_sum - u_I * J_sum + u_I * u_J * win_size
-#     I_var = I2_sum - 2 * u_I * I_sum + u_I * u_I * win_size
-#     J_var = J2_sum - 2 * u_J * J_sum + u_J * u_J * win_size
-#
-#     cc = cross * cross / (I_var * J_var + 1e-5)
-#
-#     return torch.mean(cc)
-
-
-
 class PhotoAligner(nn.Module):
     """
     Main class to perform alignment
