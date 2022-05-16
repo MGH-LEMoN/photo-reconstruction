82cc5b13514523d84f275d61aade4d7c14db626c
hvgazula <hvgazula@users.noreply.github.com>
2022-05-16 11:13:38 -0400

    added function to group jobs

--- scripts/misc_utils.py
+++ scripts/misc_utils.py
@@ -1,5 +1,6 @@
 import csv
 import glob
+import inspect
 import os
 import re
 from distutils.dir_util import copy_tree
@@ -9,10 +10,10 @@ import numpy as np
 from PIL import Image, ImageDraw, ImageFont
 from tabulate import tabulate
 
-PRJCT_KEY = 'uw_photo/Photo_data'
-PRJCT_DIR = '/space/calico/1/users/Harsha/photo-reconstruction/'
-DATA_DIR = os.path.join(PRJCT_DIR, 'data', PRJCT_KEY)
-RESULTS_DIR = os.path.join(PRJCT_DIR, 'results', PRJCT_KEY)
+PRJCT_KEY = "uw_photo/Photo_data"
+PRJCT_DIR = "/space/calico/1/users/Harsha/photo-reconstruction/"
+DATA_DIR = os.path.join(PRJCT_DIR, "data", PRJCT_KEY)
+RESULTS_DIR = os.path.join(PRJCT_DIR, "results", PRJCT_KEY)
 
 
 def grab_diff_photos(ref_string=None):
@@ -21,35 +22,37 @@ def grab_diff_photos(ref_string=None):
     Args:
         ref_string (str): options include 'image', 'hard', 'soft'
     """
-    PRJCT_KEY = 'uw_photo/Photo_data'
-    PRJCT_DIR = '/space/calico/1/users/Harsha/photo-reconstruction/'
-    DATA_DIR = os.path.join(PRJCT_DIR, 'data', PRJCT_KEY)
-    RESULTS_DIR = os.path.join(PRJCT_DIR, 'results')
+    PRJCT_KEY = "uw_photo/Photo_data"
+    PRJCT_DIR = "/space/calico/1/users/Harsha/photo-reconstruction/"
+    DATA_DIR = os.path.join(PRJCT_DIR, "data", PRJCT_KEY)
+    RESULTS_DIR = os.path.join(PRJCT_DIR, "results")
 
-    subject_list = sorted(glob.glob(os.path.join(DATA_DIR, '*')))
+    subject_list = sorted(glob.glob(os.path.join(DATA_DIR, "*")))
     subject_list = [item for item in subject_list if os.path.isdir(item)]
 
     im_list = []
-    dst_pdf = os.path.join(RESULTS_DIR, 'uw_photo_difference_images' + '.pdf')
-    for ref_string in ['image', 'hard', 'soft']:
+    dst_pdf = os.path.join(RESULTS_DIR, "uw_photo_difference_images" + ".pdf")
+    for ref_string in ["image", "hard", "soft"]:
         for skip_val in [1, 2, 3, 4]:
             for subject in subject_list:
-                ref_folder_string = f'ref_{ref_string}_skip_{skip_val}'
+                ref_folder_string = f"ref_{ref_string}_skip_{skip_val}"
                 ref_folder = os.path.join(subject, ref_folder_string)
                 diff_file = glob.glob(
-                    os.path.join(ref_folder, 'propagated_labels', '*difference*'))
+                    os.path.join(ref_folder, "propagated_labels",
+                                 "*difference*"))
 
                 if len(diff_file) == 0:
                     continue
                 src_file = diff_file[0]
 
-                print_text = os.path.basename(subject) + '_' + ref_folder_string
+                print_text = os.path.basename(
+                    subject) + "_" + ref_folder_string
                 image = Image.open(src_file)
                 draw = ImageDraw.Draw(image)
                 draw.text((image.size[0] // 2, 10),
-                        print_text,
-                        fill='white',
-                        align="right")
+                          print_text,
+                          fill="white",
+                          align="right")
                 # imagelist is the list with all image filenames
                 im_list.append(image)
 
@@ -61,7 +64,7 @@ def grab_diff_photos(ref_string=None):
 
 
 def grab_diff_photos_main():
-    for ref in ['hard', 'soft', 'image']:
+    for ref in ["hard", "soft", "image"]:
         grab_diff_photos(ref)
 
 
@@ -94,12 +97,12 @@ def print_slice_idx_all():
     """Print the index of the GT slice (excludes padding)
     Note: This info is relevant for simulating skips in reconstruction
     """
-    HENRY_RESULTS = os.path.join(DATA_DIR, 'recons/results_Henry/')
-    HARD_FOLDER = os.path.join(HENRY_RESULTS, 'Results_hard')
-    SOFT_FOLDER = os.path.join(HENRY_RESULTS, 'Results_soft')
+    HENRY_RESULTS = os.path.join(DATA_DIR, "recons/results_Henry/")
+    HARD_FOLDER = os.path.join(HENRY_RESULTS, "Results_hard")
+    SOFT_FOLDER = os.path.join(HENRY_RESULTS, "Results_soft")
 
-    hard_subject_list = sorted(glob.glob(os.path.join(HARD_FOLDER, '*-*')))
-    soft_subject_list = sorted(glob.glob(os.path.join(SOFT_FOLDER, '*-*')))
+    hard_subject_list = sorted(glob.glob(os.path.join(HARD_FOLDER, "*-*")))
+    soft_subject_list = sorted(glob.glob(os.path.join(SOFT_FOLDER, "*-*")))
 
     common_subject_list = return_common_subjects(hard_subject_list,
                                                  soft_subject_list)
@@ -108,9 +111,9 @@ def print_slice_idx_all():
     for hard_subject, soft_subject in zip(*common_subject_list):
         subject_id = os.path.basename(hard_subject)
 
-        soft_seg_gt = f'soft/{subject_id}_manualLabel.mgz'
-        soft_seg_unmerged_gt = f'soft/{subject_id}_soft_manualLabel.mgz'
-        soft_seg_merged_gt = f'soft/{subject_id}_soft_manualLabel_merged.mgz'
+        soft_seg_gt = f"soft/{subject_id}_manualLabel.mgz"
+        soft_seg_unmerged_gt = f"soft/{subject_id}_soft_manualLabel.mgz"
+        soft_seg_merged_gt = f"soft/{subject_id}_soft_manualLabel_merged.mgz"
 
         hard_seg_unmerged_str = f"{subject_id}_hard_manualLabel.mgz"
         hard_seg_merged_str = f"{subject_id}_hard_manualLabel_merged.mgz"
@@ -146,25 +149,27 @@ def print_slice_idx_all():
         # subject_gt_idx.append(temp)
         subject_gt_idx.append([subject_id] + subject_row)
 
-    with open(os.path.join(RESULTS_DIR, 'gt_slice_idx.txt'), 'w') as f:
+    with open(os.path.join(RESULTS_DIR, "gt_slice_idx.txt"), "w") as f:
         f.write(
-            tabulate(subject_gt_idx,
-                     headers=[
-                         'subject',
-                         'h_recon_shape',
-                         'h_recon_nz',
-                         'h_unmerge_shape',
-                         'h_unmerge_nz_slice',
-                         'h_merge_shape',
-                         'h_merge_nz_slice',
-                         's_recon_shape',
-                         's_recon_nz',
-                         's_unmerge_shape',
-                         's_unmerge_nz_slice',
-                         's_merge_shape',
-                         's_merge_nz_slice',
-                     ],
-                     colalign='center'))
+            tabulate(
+                subject_gt_idx,
+                headers=[
+                    "subject",
+                    "h_recon_shape",
+                    "h_recon_nz",
+                    "h_unmerge_shape",
+                    "h_unmerge_nz_slice",
+                    "h_merge_shape",
+                    "h_merge_nz_slice",
+                    "s_recon_shape",
+                    "s_recon_nz",
+                    "s_unmerge_shape",
+                    "s_unmerge_nz_slice",
+                    "s_merge_shape",
+                    "s_merge_nz_slice",
+                ],
+                colalign="center",
+            ))
 
 
 def get_gt_slice_idx(seg_vol, recon_vol=None):
@@ -179,10 +184,10 @@ def get_gt_slice_idx(seg_vol, recon_vol=None):
 
             # index of first nonzero slice (or # of padded slices)
             slice_idx_recon = np.min(
-                np.where(recon_vol[..., 0].sum(0).sum(0) > 1.))
+                np.where(recon_vol[..., 0].sum(0).sum(0) > 1.0))
             slice_idx -= slice_idx_recon
     except:
-        slice_idx = 'DNE'
+        slice_idx = "DNE"
 
     return slice_idx
 
@@ -191,11 +196,11 @@ def print_uw_gt_map():
     """Print the index of the GT slice (excludes padding)
     Note: This info is relevant for simulating skips in reconstruction
     """
-    HENRY_RESULTS = os.path.join(DATA_DIR, 'recons/results_Henry/')
-    HARD_FOLDER = os.path.join(HENRY_RESULTS, 'Results_hard')
+    HENRY_RESULTS = os.path.join(DATA_DIR, "recons/results_Henry/")
+    HARD_FOLDER = os.path.join(HENRY_RESULTS, "Results_hard")
 
     # list all folders/subjects
-    hard_subject_list = sorted(glob.glob(os.path.join(HARD_FOLDER, '*-*')))
+    hard_subject_list = sorted(glob.glob(os.path.join(HARD_FOLDER, "*-*")))
 
     subject_gt_idx = []
     for hard_subject in hard_subject_list:
@@ -212,88 +217,122 @@ def print_uw_gt_map():
         idx2 = get_gt_slice_idx(hard_seg, hard_recon)
         subject_gt_idx.append([subject_id, idx2])
 
-    with open(os.path.join(RESULTS_DIR, 'uw_gt_map.csv'), 'w') as f:
-        f.write(tabulate(subject_gt_idx, headers=[
-            'subject',
-            'gt_slice_idx',
-        ]))
+    with open(os.path.join(RESULTS_DIR, "uw_gt_map.csv"), "w") as f:
+        f.write(
+            tabulate(
+                subject_gt_idx,
+                headers=[
+                    "subject",
+                    "gt_slice_idx",
+                ],
+            ))
 
-    with open(os.path.join(RESULTS_DIR, 'uw_gt_map.csv'), 'w') as f:
+    with open(os.path.join(RESULTS_DIR, "uw_gt_map.csv"), "w") as f:
         writer = csv.writer(f)
         writer.writerows(subject_gt_idx)
 
 
 def get_hcp_subject_list():
     T1_files = glob.glob(
-        os.path.join('/space/calico/1/users/Harsha/SynthSeg/data/4harshaHCP',
-                     '*.T1.*'))
+        os.path.join("/space/calico/1/users/Harsha/SynthSeg/data/4harshaHCP",
+                     "*.T1.*"))
     T2_files = glob.glob(
-        os.path.join('/space/calico/1/users/Harsha/SynthSeg/data/4harshaHCP',
-                     '*.T2.*'))
+        os.path.join("/space/calico/1/users/Harsha/SynthSeg/data/4harshaHCP",
+                     "*.T2.*"))
 
     T1_files = [
-        re.findall('[0-9]+', os.path.basename(file))[0] for file in T1_files
+        re.findall("[0-9]+", os.path.basename(file))[0] for file in T1_files
     ]
     T2_files = [
-        re.findall('[0-9]+', os.path.basename(file))[0] for file in T2_files
+        re.findall("[0-9]+", os.path.basename(file))[0] for file in T2_files
     ]
 
     common_subjects = sorted(set(T1_files).intersection(T2_files))
 
     with open(
             os.path.join(
-                '/space/calico/1/users/Harsha/photo-reconstruction/results/hcp_subject_list.csv'
+                "/space/calico/1/users/Harsha/photo-reconstruction/results/hcp_subject_list.csv"
             ),
-            'w',
+            "w",
     ) as f:
-        f.write('\n'.join(common_subjects))
-        f.write('\n')
+        f.write("\n".join(common_subjects))
+        f.write("\n")
 
     return
 
 
 def put_skip_recons_in_main_dir():
-    subject_list = sorted(glob.glob(os.path.join(RESULTS_DIR, '*-*')))
+    subject_list = sorted(glob.glob(os.path.join(RESULTS_DIR, "*-*")))
     subject_list = [item for item in subject_list if os.path.isdir(item)]
 
     for subject in subject_list:
         subject_id = os.path.basename(subject)
         print(subject_id)
         src = subject
-        dst = os.path.join(DATA_DIR, 'Photo_data', subject_id)
+        dst = os.path.join(DATA_DIR, "Photo_data", subject_id)
         copy_tree(src, dst)
 
 
 def recon_ref_image():
-    """python version of the make target 'recon_ref_image'
-    """    
-    PRJCT_DIR= '/space/calico/1/users/Harsha/photo-reconstruction'
-    OUT_DIR=f'{PRJCT_DIR}/data/uw_photo/Photo_data'
-    DATA_DIR=f'{PRJCT_DIR}/data/uw_photo'
+    """python version of the make target 'recon_ref_image'"""
+    PRJCT_DIR = "/space/calico/1/users/Harsha/photo-reconstruction"
+    OUT_DIR = f"{PRJCT_DIR}/data/uw_photo/Photo_data"
+    DATA_DIR = f"{PRJCT_DIR}/data/uw_photo"
 
-    subjects = ['17-0333', '18-0086', '18-0444', '18-0817', '18-1045', '18-1132', '18-1196', '18-1274', '18-1327', '18-1343', '18-1470', '18-1680', '18-1690', '18-1704', '18-1705', '18-1724', '18-1754', '18-1913', '18-1930', '18-2056', '18-2128', '18-2259', '18-2260', '19-0019', '19-0037', '19-0100', '19-0138', '19-0148']
+    subjects = [
+        "17-0333",
+        "18-0086",
+        "18-0444",
+        "18-0817",
+        "18-1045",
+        "18-1132",
+        "18-1196",
+        "18-1274",
+        "18-1327",
+        "18-1343",
+        "18-1470",
+        "18-1680",
+        "18-1690",
+        "18-1704",
+        "18-1705",
+        "18-1724",
+        "18-1754",
+        "18-1913",
+        "18-1930",
+        "18-2056",
+        "18-2128",
+        "18-2259",
+        "18-2260",
+        "19-0019",
+        "19-0037",
+        "19-0100",
+        "19-0138",
+        "19-0148",
+    ]
 
-    subjects=['19-0019']
+    subjects = ["19-0019"]
     for skip in range(1, 5):
         for p in subjects:
-            command = f'python scripts/3d_photo_reconstruction.py --input_photo_dir {DATA_DIR}/Photo_data/{p}/{p}_MATLAB --input_segmentation_dir {DATA_DIR}/Photo_data/{p}/{p}_MATLAB \
+            command = f"python scripts/3d_photo_reconstruction.py --input_photo_dir {DATA_DIR}/Photo_data/{p}/{p}_MATLAB --input_segmentation_dir {DATA_DIR}/Photo_data/{p}/{p}_MATLAB \
             --ref_mask {DATA_DIR}/FLAIR_Scan_Data/{p}.rotated_masked.mgz \
             --photos_of_posterior_side --allow_z_stretch --slice_thickness 4 --photo_resolution 0.1 \
-            --output_directory {OUT_DIR}/{p}/ref_image_skip_{skip} --gpu 0 --skip  --multiply_factor {skip}'
+            --output_directory {OUT_DIR}/{p}/ref_image_skip_{skip} --gpu 0 --skip  --multiply_factor {skip}"
+
             os.system(command)
 
 
-def hcp_recon():
-    """python version of the make target 'hcp_recon'
-    """
-    PRJCT_DIR = '/space/calico/1/users/Harsha/SynthSeg/results/4harshaHCP-skip-10'
+def hcp_recon(skip=6):
+    """python version of the make target 'hcp_recon'"""
+    PRJCT_DIR = (
+        f"/space/calico/1/users/Harsha/SynthSeg/results/4harshaHCP-skip-{skip:02d}-r3"
+    )
     subjects = os.listdir(PRJCT_DIR)
-	
+
     for subject in subjects:
         input_dir = os.path.join(PRJCT_DIR, subject)
-        input_photo_dir = os.path.join(input_dir, 'photo_dir')
-        ref_mask = os.path.join(input_dir, f'{subject}.mri.mask.mgz')
-        command = f'sbatch --job-name=$$subid submit.sh scripts/3d_photo_reconstruction.py \
+        input_photo_dir = os.path.join(input_dir, "photo_dir")
+        ref_mask = os.path.join(input_dir, f"{subject}.mri.mask.mgz")
+        command = f"sbatch --job-name=$$subid submit.sh scripts/3d_photo_reconstruction.py \
 		--input_photo_dir {input_photo_dir} \
 		--input_segmentation_dir {input_photo_dir} \
 		--ref_mask {ref_mask} \
@@ -303,14 +342,91 @@ def hcp_recon():
 		--slice_thickness 8.4 \
 		--photo_resolution 0.7 \
 		--output_directory {input_dir} \
-		--gpu 0'
+		--gpu 0"
 
-        os.system(command)
+        print(command)
 
     pass
 
-if __name__ == '__main__':
-    grab_diff_photos()
+
+def hcp_recon_group(skip=6, n_jobs=5):
+    """python version of the make target 'hcp_recon' to group multiple reconstructions at once"""
+    PRJCT_DIR = (
+        f"/space/calico/1/users/Harsha/SynthSeg/results/4harshaHCP-skip-{skip:02d}-r3"
+    )
+    subjects = sorted(os.listdir(PRJCT_DIR))
+    LOG_DIR = f"/space/calico/1/users/Harsha/photo-reconstruction/logs/hcp-recon/test-skip-{skip:02d}-r3"
+
+    os.makedirs(LOG_DIR, exist_ok=True)
+
+    if not n_jobs:
+        raise Exception('Number of jobs grouped cannot be None or Zero')
+
+    for idx, ptr in enumerate(range(0, len(subjects) - n_jobs + 1, n_jobs)):
+        script_file = os.path.join(os.getcwd(), "submit_scripts",
+                                   f"submit_group_{idx:02d}.sh")
+        batch = subjects[ptr:ptr + n_jobs]
+
+        all_commands = []
+        for subject in batch:
+            log_file = os.path.join(LOG_DIR, subject + ".out")
+            input_dir = os.path.join(PRJCT_DIR, subject)
+            input_photo_dir = os.path.join(input_dir, "photo_dir")
+            ref_mask = os.path.join(input_dir, f"{subject}.mri.mask.mgz")
+            command = f"python scripts/3d_photo_reconstruction.py \
+            --input_photo_dir {input_photo_dir} \
+            --input_segmentation_dir {input_photo_dir} \
+            --ref_mask {ref_mask} \
+            --photos_of_posterior_side \
+            --allow_z_stretch \
+            --order_posterior_to_anterior \
+            --slice_thickness {0.7*skip:.1f} \
+            --photo_resolution 0.7 \
+            --output_directory {input_dir}\
+            --gpu 0 > {log_file} &"
+
+            command = " ".join(command.split())
+            all_commands.append(command)
+
+        sbatch_top = """#!/bin/bash
+                #SBATCH --account=lcnrtx
+                #SBATCH --partition=rtx6000
+                #SBATCH --nodes=1
+                #SBATCH --ntasks=5
+                #SBATCH --gpus=1
+                #SBATCH --time=0-00:30:00
+                #SBATCH --output="./logs/hcp-recon/test/%x.out"
+                #SBATCH --error="./logs/hcp-recon/test/%x.err"
+                #SBATCH --mail-user=hvgazula@umich.edu
+                #SBATCH --mail-type=FAIL
+
+                source /space/calico/1/users/Harsha/venvs/recon-venv/bin/activate
+                export PYTHONPATH=/space/calico/1/users/Harsha/photo-reconstruction
+
+                echo 'Start time:' `date`
+                echo 'Node:' $HOSTNAME
+                echo "$@"
+                start=$(date +%s)
+                """
+
+        sbatch_bottom = """end=$(date +%s)
+                        echo 'End time:' `date`
+                        echo "Elapsed Time: $(($end-$start)) seconds"
+                        """
+
+        with open(script_file, "w+") as f:
+            f.write(inspect.cleandoc(sbatch_top))
+            f.write("\n" * 2)
+            f.write("\n".join(all_commands))
+            f.write("\n")
+            f.write("wait")
+            f.write("\n" * 2)
+            f.write(inspect.cleandoc(sbatch_bottom))
+
+
+if __name__ == "__main__":
+    # grab_diff_photos()
     # put_skip_recons_in_main_dir()
     # print_uw_gt_map()
     # get_hcp_subject_list()
+    hcp_recon_group(skip=12, group_size=5)
